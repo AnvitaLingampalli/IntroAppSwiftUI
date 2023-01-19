c775c5ae32dc5548ddecb7d1c731ba7e260da243
AnvitaLingampalli <lingampalligunny@gmail.com>
2023-01-18 19:14:19 -0700

    Initial Commit

---
+++ IntroAppSwiftUI/IntroAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroAppSwiftUIApp.swift
+//  IntroAppSwiftUI
+//
+//  Created by Anvita Lingampalli on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct IntroAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
